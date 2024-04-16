a1ede358009b1b4becfa999ed8cba1175ad04d0d
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-04-16 16:48:24 +0530

    portfolio

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+
+navToggle.addEventListener('click', function () {
+    links.classList.toggle('show-links');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times');
+    const isMenuOpen = links.classList.contains('show-links');
+});
+
+closeIcon.addEventListener('click', function () {
+    links.classList.remove('show-links');
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+});
